d8b0407f03c454c6aeb07e985a0f96cce294018e
Satya Sandeep <satyasandeep786@gmail.com>
2020-03-07 01:12:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,48 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 let routes = require('./routes');
+const bodyParser = require("body-parser");
+const compress = require("compression");
+const methodOverride = require("method-override");
+const helmet = require("helmet");
+const bearerToken = require('express-bearer-token');
+
+const accessControlAllow = require("./middlewares/accessControlAllow");
+
+// // // Init Middleware
+// app.use(cors());
+// app.use(express.json({ extended: false }));
+// app.use(accessControlAllow.allow);
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(morgan('dev'))
+
+// gzip compression
+app.use(compress());
+
+// lets you use HTTP verbs such as PUT or DELETE
+// in places where the client doesn't support it
+app.use(methodOverride());
+
+// secure apps by setting various HTTP headers
+app.use(helmet());
+
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+// add token middleware in the express application
+app.use(bearerToken());
+
+
 
 require('dotenv').config();
 
 
 const app = express();
 const port = process.env.PORT || 8000;
-
-app.use(cors());
-app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
